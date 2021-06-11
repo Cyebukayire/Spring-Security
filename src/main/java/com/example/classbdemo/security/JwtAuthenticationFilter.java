a4c37109ae0efacad0c0c75854241eb9f368391a
Cyebukayire <peacecyebukayire@gmail.com>
2021-06-11 16:40:21 +0200

    token provider

--- src/main/java/com/example/classbdemo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/classbdemo/security/JwtAuthenticationFilter.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
