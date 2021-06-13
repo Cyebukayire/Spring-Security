88dd1d204110d55d701c2900de7442be945e0a8c
Cyebukayire <peacecyebukayire@gmail.com>
2021-06-13 15:39:53 +0200

    Mark Repository

--- src/main/java/com/example/classbdemo/repositories/MarkRepository.java
+++ src/main/java/com/example/classbdemo/repositories/MarkRepository.java
@@ -1,18 +1,11 @@
 package com.example.classbdemo.repositories;
 
-import com.example.classbdemo.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.classbdemo.model.Mark;
 
-import java.util.Optional;
-
 @Repository
 public interface MarkRepository extends JpaRepository<Mark, Long> {
-
-    boolean existsByScoredAndStudentAndCourse(int Scored, String student, String course);
-
-    Optional<Course> findByScoredAndStudentAndCourse(int Scored, String student, String course);
 
 }
